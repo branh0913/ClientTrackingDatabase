684ea7b0a3dc9449a71de5516d7054d64754f328
Brandon <branh0913@live.com>
2015-12-01 22:54:07 -0500

    Changed around a few things in the model file

--- Model.py
+++ Model.py
@@ -4,42 +4,38 @@ from sqlalchemy.orm import relationship
 
 __author__ = 'Brandon'
 
-
-
 Base = declarative.declarative_base()
 
-class Insurance(Base):
 
+class Insurance(Base):
     __tablename__ = "Insurance"
 
-    Insurance_Id = Column('Id', Integer, primary_key=True)
-    Insurance_Name = Column('InsuranceName', String(40))
+    id = Column(Integer, primary_key=True)
+    Insurance_Name = Column(String(40))
+
 
 class Patient(Base):
-
     __tablename__ = "Patient"
 
-    first_name = Column("FirstName", String(20))
-    last_name = Column("LastName", String(20))
-    patient_id = Column("Id", Integer, primary_key=True)
+    first_name = Column(String(20))
+    last_name = Column(String(20))
+    id = Column(Integer, primary_key=True)
+
 
 class Client(Base):
-
     __tablename__ = "Client"
 
-    client_Id = Column('Id', Integer, primary_key=True)
-    patient_Id = Column('PatientId', Integer, ForeignKey('Patient.Id'), nullable=False)
+    id = Column(Integer, primary_key=True)
+    patient_Id = Column(Integer, ForeignKey('Patient.id'), nullable=False)
 
-    insurance_Id = Column('InsuranceId', Integer, ForeignKey('Insurance.Id'), nullable=False)
+    insurance_Id = Column(Integer, ForeignKey('Insurance.id'), nullable=False)
     insurance = relationship(Insurance)
 
-    priority = Column('Priority', String(10))
-    due_date = Column('DueDate', Date)
-    todays_date = Column('TodaysDate', Date)
-    summary = Column('Summary', String(100))
-    note = Column('Note', String(100))
-
-
+    priority = Column(String(10))
+    due_date = Column(Date)
+    todays_date = Column(Date)
+    summary = Column(String(100))
+    note = Column(String(100))
 
 
 customer_tracker = create_engine('sqlite:///customerTrackerDB.db')
